b2c4ee296cb730609e44470d08331adeed94cff7
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-07-25 08:51:48 -0400

    Add an index route, add notes about why what's happening is happening

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,21 @@
 import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
+  /**
+   * This model hook is ran for _every_ route. Because routes are a hierarchy.
+   * But because it would be verbose to say 'application.foo', application is
+   * omitted from all route names (dot notation).
+   *
+   * So, we _cannot_ transition away from the application route. I believe this
+   * should probably throw an error when transitionTo is called at all from the
+   * application route.
+   *
+   * Instead, what we want to do is transition away *only* if we are not on a sub-route.
+   * Like, application.index.
+   */
   async model() {
     console.log('Application Model Hook');
-    return this.transitionTo('foo');
+    // NullVoxPopuli's opinion: this should throw an error
+    // return this.transitionTo('foo');
   }
 }
