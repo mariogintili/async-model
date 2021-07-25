d6bbc638505dca34a6dad8d6bac27fa78456c9df
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-07-25 08:45:16 -0400

    Observe Foo#model is not ran

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   async model() {
+    console.log('Application Model Hook');
     return this.transitionTo('foo');
-  }
+  },
 });
--- app/routes/foo.js
+++ app/routes/foo.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class FooRoute extends Route {
+  async model() {
+    console.log('Foo Model Hook');
+  }
 }
